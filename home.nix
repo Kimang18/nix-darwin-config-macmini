a6ee6ac6c65223397c222dbc2a5cbf2a7c5e9141
Kimang18 <allenkhun22@gmail.com>
2026-05-28 10:59:26 +0200

    update configfile

--- home.nix
+++ home.nix
@@ -1,8 +1,39 @@
 { config, pkgs, ... }: {
   # this is internal compatibility configuration
   # for home-manager, dont change this!
-  home.stateVersion = "23.05";
+  home.stateVersion = "25.05";
   # Let home-manager install and manage itself.
+  # config file setup
+  xdg.configFile = {
+    "aerospace/aerospace.toml" = {
+      source = ./aerospace_configuration;
+      force = true;
+    };
+    # ".config/kitty/gruvbox_cs/gruvbox_dark.conf".source = ./kitty/gruvbox_cs/gruvbox_dark.conf;
+    # ".config/kitty/gruvbox_cs/gruvbox_light.conf".source = ./kitty/gruvbox_cs/gruvbox_light.conf;
+    # ".config/kitty/tokyonight_night.conf".source = ./kitty/tokyonight_night.conf;
+    "fish/functions/fish_prompt.fish" = {
+      source = ./fish_prompt.fish;
+      force = true;
+    };
+    "nvim/init.lua" = {
+      source = ./init.lua;
+      force = true;
+    };
+    "tmux/tmux.conf" = {
+      source = ./tmux.conf;
+      force = true;
+    };
+    "ghostty/config" = {
+      source = ./ghostty.conf;
+      force = true;
+    };
+    "zathura/zathurarc" = {
+      source = ./zathura_configuration;
+      force = true;
+    };
+  };
+
   programs.home-manager.enable = true;
   home.packages = with pkgs; [
     (pkgs.python3.withPackages (ppkgs: [
@@ -18,20 +49,20 @@
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-  programs.vim = {
-    enable = true;
-    extraConfig = builtins.readFile(./vim_configuration);
-    plugins = with pkgs.vimPlugins; [
-      gruvbox
-      nerdtree
-      vim-nix
-      vim-airline
-      vim-airline-themes
-      vim-gitgutter
-      vim-devicons
-      YouCompleteMe
-    ];
-  }; 
+  # programs.vim = {
+  #   enable = true;
+  #   extraConfig = builtins.readFile(./vim_configuration);
+  #   plugins = with pkgs.vimPlugins; [
+  #     gruvbox
+  #     nerdtree
+  #     vim-nix
+  #     vim-airline
+  #     vim-airline-themes
+  #     vim-gitgutter
+  #     vim-devicons
+  #     YouCompleteMe
+  #   ];
+  # }; 
 
 programs.vscode = {
     enable = true;
@@ -47,11 +78,6 @@ programs.vscode = {
     # };
   };
 
-  # neovim setup but not working yet
-  #programs.neovim = {
-  #  enable = true;
-  #  extraConfig = builtins.readFile(./init.lua);
-  #};
   programs.neovim = {
     enable = true;
   };
@@ -69,8 +95,8 @@ programs.vscode = {
         mgr.append_keymap = [
           {
             on =[ "g" "p" ];
-            run = "cd ~/Documents/01_Projects/lora-demo/";
-            desc = "Go Lora Demo";
+            run = "cd ~/Documents/01_Projects";
+            desc = "Go to project folders";
           }
         ];
       };
@@ -106,51 +132,6 @@ programs.vscode = {
       cpu
       battery
     ];
-    # terminal = "screen-256color";
-    extraConfig = ''
-      set-option -g status-position top
-      set -g default-command fish
-      set-option -g prefix C-q
-      unbind-key C-q
-      bind-key C-q send-prefix
-      set -g mouse on
-      set-option -g default-terminal 'tmux-256color'
-      set-option -g terminal-overrides ',xterm-256color:RGB'
-      bind -n M-Left select-pane -L
-      bind -n M-Right select-pane -R
-      bind -n M-Up select-pane -U
-      bind -n M-Down select-pane -D
-      bind-key v split-window -h
-      bind-key h split-window -v
-      bind-key u split-window "aerospace focus --window-id $(aerospace list-windows --all | fzf --height 40% --color 'border:#ffa07a,info:#ffff00' --border | cut -d'|' -f1 | tr -d '[:blank:]')"
-      bind -n S-Left previous-window
-      bind -n S-Right next-window
-      set -g renumber-windows "on"
-      set -g @continuum-restore "on"
-      set -g @resurrect-strategy-nvim 'session'
-      set -g @catppuccin_flavor "mocha"
-      set -g @catppuccin_window_status_style "rounded"
-      set -g @catppuccin_window_number_position "right"
-      set -g @catppuccin_window_default_text "#W"
-      set -g @catppuccin_window_current_fill "number"
-      set -g @catppuccin_window_current_text "#W"
-      set -g @catppuccin_date_time_text "%d.%m. %H:%M"
-
-      set -g status-right-length 100
-      set -g status-left-length 100
-      set -g status-left "#{E:@catppuccin_status_session} "
-      set -g status-right "#{E:@catppuccin_status_application}"
-      set -agF status-right "#{E:@catppuccin_status_cpu}"
-      set -ag status-right "#S #[fg=#{@thm_fg},bg=#{@thm_mantle}] #(tmux-mem-cpu-load --interval 2 | cut -d ' ' -f1 )#[default]"
-      set -ag status-right "#{E:@catppuccin_status_uptime}"
-      set -ag status-right "#{E:@catppuccin_status_date_time}"
-      set -agF status-right "#{E:@catppuccin_status_battery}"
-      run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
-      set-window-option -g mode-keys vi
-      bind-key -T copy-mode-vi v send -X begin-selection
-      bind-key -T copy-mode-vi v send -X selection-line
-      bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
-    '';
   };
 
   programs.fish = {
@@ -171,8 +152,8 @@ programs.vscode = {
 
   # kitty setup
   programs.kitty = {
-    enable = true;
-    extraConfig = builtins.readFile(./kitty_configuration);
+    enable = false;
+    # extraConfig = builtins.readFile(./kitty_configuration);
   };
 
   # git setup
@@ -183,16 +164,4 @@ programs.vscode = {
     ignores = [ ".DS_Store" ];
   };
 
-  # config file setup
-  home.file = {
-    ".aerospace.toml".source = ./aerospace_configuration;
-    ".config/kitty/gruvbox_cs/gruvbox_dark.conf".source = ./kitty/gruvbox_cs/gruvbox_dark.conf;
-    ".config/kitty/gruvbox_cs/gruvbox_light.conf".source = ./kitty/gruvbox_cs/gruvbox_light.conf;
-    ".config/kitty/tokyonight_night.conf".source = ./kitty/tokyonight_night.conf;
-    ".config/fish/functions/fish_prompt.fish".source = ./fish_prompt.fish;
-    ".config/nvim/init.lua".source = ./init.lua;
-    # ".config/nvim/diary_template.md".source = ./diary_template.md;
-    ".config/ghostty/config".source = ./ghostty.conf;
-    ".config/zathura/zathurarc".source = ./zathura_configuration;
-  };
 }
